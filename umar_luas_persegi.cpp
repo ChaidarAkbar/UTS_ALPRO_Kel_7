a89ccd38f09751c09adb18b5cc9fb7ea1f7aa760
oemaremdeh <oemaremdeh@gmail.com>
2025-11-04 20:51:47 +0700

    Add program to calculate square area

---
+++ umar_luas_persegi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   double sisi, luas;
+    cout << "Masukkan panjang sisi persegi : ";
+    cin >> sisi;
+
+    luas = sisi * sisi;
+    cout << "Luas Persegi : " << luas ;
+
+    return 0;
+}
